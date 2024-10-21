db0626fb8f25bc8eb846d8db8d5a189e84bc1106
senju42 <142969130+senju42@users.noreply.github.com>
2024-10-21 15:35:38 +0200

    abgabe

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,37 @@
-"use strict"
-// opendata api URL
-const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
+"use strict";
+const stationElement = document.getElementById("station");
+const buttonElement = document.getElementById("submit");
+const tableElement = document.getElementById("time-table");
 
-const getData = async () => {};
+buttonElement.addEventListener("click", async () => {
+  const data = await getData();
+  displayData(data);
+});
 
-const displayData = (data) => {};
+const getData = async () => {
+  const station = stationElement.value;
+  const url = `https://transport.opendata.ch/v1/stationboard?station=${station}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const displayData = (data) => {
+    const stationName = data.station.name;
+    const stationNameElement = document.getElementById("station-name");
+    stationNameElement.textContent = stationName;
+    data.stationboard.forEach((train) => {
+        const gleis = train.passList[0].platform;
+        const destination = train.to;
+        const time = new Date(train.stop.departure);
+        const timeString = time.toLocaleTimeString("de-ch");
+
+        const row = tableElement.insertRow();
+        const cellUhrzeit = row.insertCell();
+        cellUhrzeit.textContent = timeString;
+        const cellZeit = row.insertCell();
+        cellZeit.textContent = destination;
+        const cellGleis = row.insertCell();
+        cellGleis.textContent = gleis;
+    });
+};
